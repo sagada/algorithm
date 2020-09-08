b17c01c32556fa147c2063787712e7d00b355344
ysh <wlatmdqja@naver.com>
2020-09-08 22:41:01 +0900

    백준 문제집

---
+++ backjoon/1766/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+int n;
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+    
+}
