ecaf2652920090492ff0137c14de2f9f66901b69
sagada <wlatmdqja@naver.com>
2022-02-11 02:32:18 +0900

    14719 수정

--- backjoon/14719.java
+++ backjoon/14719.java
@@ -4,44 +4,32 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int w = scanner.nextInt();
+		int [] map = new int[w];
+		int result = 0;
 
-	    int h, w;
+		for (int i = 0; i < w; i++)
+			map[i] = scanner.nextInt();
 
-	    h = scanner.nextInt();
-	    w = scanner.nextInt();
-	    int [] map = new int[w];
-
-	    int result = 0;
-	    for (int i = 0; i < w; i++)
-        {
-            map[i] = scanner.nextInt();
-        }
-
-	    for (int i = 1; i < w - 1; i++)
+		for (int i = 1; i < w - 1; i++)
 		{
 			int leftMax = 0;
 			int rightMax = 0;
 
 			for (int k = 0; k <= i - 1; k ++)
-			{
 				leftMax = Math.max(leftMax, map[k]);
-			}
 
 			for (int j = i + 1; j < w; j++)
-			{
 				rightMax = Math.max(rightMax, map[j]);
-			}
 
 			int curHeight = map[i];
-			if (curHeight < Math.min(rightMax, leftMax))
-			{
-				result += Math.min(rightMax, leftMax) - curHeight;
-			}
+			int minHeight = Math.min(rightMax, leftMax);
+			if (curHeight < minHeight)
+				result += minHeight - curHeight;
 
 		}
-
 		System.out.println(result);
-    }
+	}
 }
