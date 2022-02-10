223e614b50ff3fef57767eaa31540c2f5f95e1db
sagada <wlatmdqja@naver.com>
2022-02-11 02:21:10 +0900

    14719

---
+++ backjoon/14719.java
@@ -0,0 +1,47 @@
+package com.algo.ri;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+
+	    int h, w;
+
+	    h = scanner.nextInt();
+	    w = scanner.nextInt();
+	    int [] map = new int[w];
+
+	    int result = 0;
+	    for (int i = 0; i < w; i++)
+        {
+            map[i] = scanner.nextInt();
+        }
+
+	    for (int i = 1; i < w - 1; i++)
+		{
+			int leftMax = 0;
+			int rightMax = 0;
+
+			for (int k = 0; k <= i - 1; k ++)
+			{
+				leftMax = Math.max(leftMax, map[k]);
+			}
+
+			for (int j = i + 1; j < w; j++)
+			{
+				rightMax = Math.max(rightMax, map[j]);
+			}
+
+			int curHeight = map[i];
+			if (curHeight < Math.min(rightMax, leftMax))
+			{
+				result += Math.min(rightMax, leftMax) - curHeight;
+			}
+
+		}
+
+		System.out.println(result);
+    }
+}
